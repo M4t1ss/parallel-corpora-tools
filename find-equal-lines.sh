345dc058ecf38cd5c93742b1ff286ab68ba91d6b
Matīss Rikters <matiss_rikters@hotmail.com>
2017-12-06 00:11:04 +0200

    Advertise some more

--- find-equal-lines.sh
+++ find-equal-lines.sh
@@ -16,6 +16,12 @@ rm $1.s
 rm $2.s
 
 # Now we can remove the equal lines from the original parallel files
-# But this is crazily slow!! Better use MP's FilterTestDataFromTrainingData.exe
+
 grep -v -x -F -f $1.equal.txt $1 > $1.c
 grep -v -x -F -f $1.equal.txt $2 > $2.c
+
+# But this is crazily slow!! Better use MP's FilterTestDataFromTrainingData.exe (needs sudo apt-get -qq -y install mono-complete)
+#
+# 	mono --runtime=v4.0 FilterTestDataFromTrainingData.exe \
+#		-P filename.en.equal.txt filename.en.equal.txt filename.en.equal.txt filename.en.equal.txt \
+#		filename.en filename.de filename.en.c filename.de.c
