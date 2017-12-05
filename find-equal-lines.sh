a016f814b9537693a166eccd5d07067ae253a28a
Matīss Rikters <matiss_rikters@hotmail.com>
2017-12-05 23:09:05 +0200

    Clean as well...

--- find-equal-lines.sh
+++ find-equal-lines.sh
@@ -1,6 +1,11 @@
 #/bin/bash
-# Parameters - parallel file names filename1 and filename2
-# Output - equal lines found in both files - filename1.equal.txt
+# Parameters - parallel file names
+#	filename.en
+#	filename.de
+# Outputs - equal lines found in both files and cleaned parallel files
+#	filename.en.equal.txt
+#	filename.en.c
+#	filename.de.c
 
 sort $1 > $1.s
 sort $2 > $2.s
@@ -9,3 +14,8 @@ comm -12 $1.s $2.s > $1.equal.txt
 
 rm $1.s
 rm $2.s
+
+# Now we can remove the equal lines from the original parallel files
+# But this is crazily slow!! Better use MP's FilterTestDataFromTrainingData.exe
+grep -v -x -F -f $1.equal.txt $1 > $1.c
+grep -v -x -F -f $1.equal.txt $2 > $2.c
