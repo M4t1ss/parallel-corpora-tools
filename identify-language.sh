6729709e253d5c6b3a419bf18abdf30e96bc2d29
Matīss Rikters <matiss_rikters@hotmail.com>
2017-12-06 11:02:19 +0200

    uncomment

--- identify-language.sh
+++ identify-language.sh
@@ -9,12 +9,15 @@
 #	en
 #	de
 #
-# Outputs - files with language tags & confidences per line
+# Outputs - files with language tags & confidences per line; bad line numbers; filtered parallel files
 #	filename.en.lang.txt
 #	filename.de.lang.txt
+#	filename.en.lang.txt.filename.de.lang.txt.bad
+#	filename.en.lang.txt.goodlang
+#	filename.de.lang.txt.goodlang
 
-# langid --line -n < $1 > $1.lang.txt
-# langid --line -n < $2 > $2.lang.txt
+langid --line -n < $1 > $1.lang.txt
+langid --line -n < $2 > $2.lang.txt
 
 # Get the IDs of the bad sentences
 php find-bad.php $1.lang.txt $2.lang.txt $3 $4
