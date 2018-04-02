eddaf12b6cbfb50863707e6c5fb6e866225f41dd
Matīss Rikters <matiss_rikters@hotmail.com>
2018-04-02 16:36:13 +0300

    Added languages in regular expression file

--- regular-expressions.php
+++ regular-expressions.php
@@ -2,3 +2,5 @@
 $regex['lv'] = "a-zA-ZāčēģīķļņšūžĀČĒĢĪĶĻŅŠŪŽ";
 $regex['et'] = "a-zA-ZäõöšüžÄÕÖŠÜŽ";
 $regex['en'] = "a-zA-Z";
+$regex['fi'] = "a-zA-ZäåöšžÄÅÖŠŽ";
+$regex['tr'] = "a-zA-ZçğöşüÇĞÖŞÜ";
