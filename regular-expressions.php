8684931bc2ef15a4c54095289a78108bbc0ca6fa
Matīss Rikters <matiss_rikters@hotmail.com>
2018-04-16 08:37:08 +0300

    + regexes

--- regular-expressions.php
+++ regular-expressions.php
@@ -2,3 +2,6 @@
 $regex['lv'] = "a-zA-ZāčēģīķļņšūžĀČĒĢĪĶĻŅŠŪŽ";
 $regex['et'] = "a-zA-ZäõöšüžÄÕÖŠÜŽ";
 $regex['en'] = "a-zA-Z";
+$regex['fi'] = "a-zA-ZäåöšžÄÅÖŠŽ";
+$regex['tr'] = "a-zA-ZçğöşüÇĞÖŞÜ";
+$regex['de'] = "a-zA-ZäöüßÄÖÜẞ";
