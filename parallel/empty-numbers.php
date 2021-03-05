bafbe146030e810c3e86a0e756dc001027d5058e
Matīss Rikters <matiss_rikters@hotmail.com>
2021-03-05 16:17:31 +0900

    A couple more filters
    A couple more filters that return only bad sentence numbers

---
+++ parallel/empty-numbers.php
@@ -0,0 +1,42 @@
+﻿<?php
+error_reporting(E_ALL & ~E_WARNING);
+
+//Input parameters
+$source_sentences 	= $argv[1];
+$target_sentences	= $argv[2];
+
+//Open files
+$inSRC = fopen($source_sentences, "r") or die("Can't open source input file!");
+$inTRG = fopen($target_sentences, "r") or die("Can't open target input file!");
+
+$outSRC = fopen($source_sentences.".badnumbers", "w") or die("Can't create source output file!");
+$badNumberArray = [];
+
+$i = $j = $k = 0;
+$line = 1;
+while (($sourceSentence = fgets($inSRC)) !== false && ($targetSentence = fgets($inTRG)) !== false) {
+	
+	$sourceSentence = trim($sourceSentence);
+	$targetSentence = trim($targetSentence);
+	
+	// More than half are non-alpha
+	if(strlen($sourceSentence) == 0 || strlen($targetSentence) == 0)
+	{
+		if(!in_array($line, $badNumberArray)){
+			$badNumberArray[] = $line;
+			$i++;
+		}
+	}
+	
+	$line++;
+}
+
+echo "Found ".$i." empty lines on both sides\n";
+
+foreach($badNumberArray as $badNumber){
+	fwrite($outSRC, $badNumber."\n");
+}
+
+fclose($inSRC);
+fclose($inTRG);
+fclose($outSRC);
